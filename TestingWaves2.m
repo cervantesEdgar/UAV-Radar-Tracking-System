9594da842c88cb89fd5088de5dd1b5b728bd82cc
Edgar Cervantes <edgar.cervantes@vallartasupermarkets.com>
2024-05-08 15:20:33 -0700

    Updating Wave Testing

--- TestingWaves2.m
+++ TestingWaves2.m
@@ -1,23 +1,17 @@
-clc,clear,close
+clear, close, clc
 
-fs = 40;   % Sampling Frequency
-T = 1/fs;  % Sampling Period
-N = 40;    % Length of Signal
-t = (0:N-1)*T; % Time Vector
-
-fc = 3;    % Frequency
-Xn = sin(2*pi*fc*t);
-
-plot(t,Xn,'.-', 'MarkerSize',20);
-title("Signal")
-xlabel("t (Time)")
-ylabel("Xn(t)")
-
-Y = fft(Xn);
-k = 0:N/2;
-freq = k*fs/N;
+Fs = 2000;
+t = 0:1/Fs:1;
+f = 200;
 
 figure
-stem(freq,abs(Y(1:(N/2)+1)));
-title("One Side Absolute Value");
-xlabel("Frequency (Hz)");
+subplot(2,1,1)
+
+s1 = sin(2*pi*f*t);
+plot(t,s1)
+xlabel('Time (seconds)');
+ylabel('Amplitude');
+title('Time Domain Plot')
+
+subplot(2,1,2)
+spectrogram(s1,256,250,260,2E3,'yaxis')
